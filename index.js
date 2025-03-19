2d7ef2789faa676649aa48597995ae7921e6e432
Victor <aue228lublupacanov@gmail.com>
2025-03-19 16:38:16 +0100

    cors access added for frontend

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const Minio = require('minio')
 
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(cors())
 
 const minioClient = new Minio.Client({
     endPoint: '16.16.66.55',
