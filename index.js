a06ee764714924ab1f27bf5f7c12d8dc0655f79b
Victor <aue228lublupacanov@gmail.com>
2025-03-19 16:15:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const Minio = require('minio')
+
+const app = express()
+const port = 3000
+
+const minioClient = new Minio.Client({
+    endPoint: '16.16.66.55',
+    port: 9000,
+    useSSL: false,
+    accessKey: 'minioadmin',
+    secretKey: 'minioadmin'
+})
+
+minioClient.bucketExists('boss-worker-bucket', function (err, exists) {
+    if (err) {
+        return console.log('Error connect to Minio', err)
+    }
+    if (exists) {
+        console.log('Connected to Minio!!:)')
+    } else {
+        console.log("Bucket does not exist")
+    }
+})
+
+app.get('/list/:bucket', (req, res) => {
+    const { bucket } = req.params
+    const objectList = []
+
+    const stream = minioClient.listObjects(bucket, '', true)
+
+    stream.on('data', (obj) => {
+        objectList.push(obj.name)
+    })
+
+    stream.on('end', () => {
+        res.json({ files: objectList })
+    })
+
+    stream.on('error', (err) => {
+        res.status(500).send('Error to get list of files: ' + err.message)
+    })
+})
+
+app.get('/download/:bucket/:object', (req, res) => {
+    const { bucket, object } = req.params
+
+    minioClient.getObject(bucket, object, (err, dataStream) => {
+        if (err) {
+            return res.status(500).send('Error with receiving data: ' + err.message)
+        }
+
+        res.setHeader('Content-Disposition', `attachment; filename=${object}`)
+        res.setHeader('Content-Type', 'application/octet-stream')
+
+        dataStream.pipe(res)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
